c0e1d8cb0cf6ab6e0d4fbb2f81b6c9e030f72692
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-09-09 15:30:08 +0200

    Project setup

---
+++ src/test/java/se/lexicon/E_commerce_platform/ECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.E_commerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
